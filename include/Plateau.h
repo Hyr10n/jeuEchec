02927fe0168d37ab25471b915c49264df6e6b0ed
Briac <briac.hillion@epsi.fr>
2020-04-10 10:31:26 +0200

    Plateau avec entier

---
+++ include/Plateau.h
@@ -0,0 +1,62 @@
+#ifndef PLATEAU_H
+#define PLATEAU_H
+#include <vector>
+#include <cstdlib>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+class Piece{
+
+
+
+
+
+
+
+
+};
+
+
+
+
+class Plateau
+{
+
+     private:
+        vector<int> tableau;
+        int largeur;
+        int hauteur;
+        int x;
+        int y;
+        int value;
+        vector<Piece*> pieces;
+
+
+    public:
+        Plateau(int largeur, int hauteur){
+        this->largeur = largeur;
+        this->hauteur = hauteur;
+        tableau.resize(hauteur * largeur, 0);
+        }
+        set(int x, int y, int value){
+        this->x = x;
+        this->y = y;
+        this->tableau[this->getIndice(x, y)] = value;
+
+        }
+
+        int getIndice(int x, int y);
+        int getLargeur();
+        int getHauteur();
+
+
+    int get(int x, int y);
+
+    void afficher();
+
+
+
+};
+
+#endif // PLATEAU_H
--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include "Plateau.h"
+#include <vector>
+#include <cstdlib>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    cout << "Je d'echec!" << endl;
+    Plateau plateau(8, 8);
+    plateau.set(1, 1, 1);
+    plateau.set(2, 2, 6);
+    plateau.afficher();
+
     return 0;
 }
+
